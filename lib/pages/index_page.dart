f5c8d8e0898ff54613bfb824d1db23db1de727f2
zyj <yjzhangay@isoftstone.com>
2020-08-28 10:09:44 +0800

    1

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping'),
+      ),
+      body: Center(
+        child: Text('This is shopping'),
+      ),
+    );
+  }
+}
