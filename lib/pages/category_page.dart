6f81761155c65d1a63f3b452860614d9c3a8c1ce
zyj <zhangyajun880828@gmail.com>
2020-08-31 18:04:07 +0800

    home

---
+++ lib/pages/category_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryPage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text('category'),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+class HomePage extends StatefulWidget {
+
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+
+  }
+}
